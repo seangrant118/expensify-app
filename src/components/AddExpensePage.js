ad35141694c4d928c5f03045a347a460bd8f21f7
Sean Grant <seangrant118@gmail.com>
2020-02-04 09:39:54 -0500

    revert

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses.js';
+import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
