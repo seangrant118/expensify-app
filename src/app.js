0e74555c8c9b99e5c2e0e4f16cc0dbf232fed18f
Sean Grant <seangrant118@gmail.com>
2020-02-13 14:12:25 -0500

    Redirecting Login or Logout

--- src/app.js
+++ src/app.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './Store/configureStore';
-import { startSetExpenses } from './Actions/expenses';
-import { setTextFilter } from './Actions/filters';
-import getVisibleExpenses from './Selectors/expenses';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter, { history } from "./routers/AppRouter";
+import configureStore from "./Store/configureStore";
+import { startSetExpenses } from "./Actions/expenses";
+import { setTextFilter } from "./Actions/filters";
+import getVisibleExpenses from "./Selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById("app"));
+    hasRendered = true
+  }
+}
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-})
-
-firebase.auth().onAuthStateChanged((user) => {
+firebase.auth().onAuthStateChanged(user => {
   if (user) {
-    console.log('log in')
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard')
+      }
+    });
   } else {
-    console.log('log out')
+    renderApp();
+    history.push("/");
   }
 });
